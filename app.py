fb6735b1ab953f67cd695130640416e3028c4d4f
billalcodes <bilalsarwar4441278@gmail.com>
2026-07-01 14:17:40 +0500

    update the program

--- app.py
+++ app.py
@@ -1,4 +1,42 @@
-num = int(input("Number enter karein: "))
+# Tables
+# num = int(input("Number enter karein: "))
 
-for i in range(1, 11):
-    print(f"{num} x {i} = {num * i}")g
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num * i}")
+
+
+    # calculater
+
+# a = int(input("First number: "))
+# b = int(input("Second number: "))
+
+# print("Addition :",a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Division:", a / b)
+# even oddd
+# num = int(input("Number: "))
+
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+# n = int(input("N: "))
+# total = 0
+
+# for i in range(1, n + 1):
+#     total += i
+
+# print("Sum:", total)
+# factorial
+# n = int(input("Number: "))
+# fact = 1
+
+# for i in range(1, n + 1):
+#     fact = fact * i
+
+# print("Factorial:", fact)
+
+
+
